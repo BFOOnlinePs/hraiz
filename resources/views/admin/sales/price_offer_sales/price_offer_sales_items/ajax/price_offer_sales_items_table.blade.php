ff58458f13658cefe52359b1207c54bedfd23132
AbuMaraqa <maraqamohamad@outlook.sa>
2024-05-23 12:08:15 +0300

    jkl

--- resources/views/admin/sales/price_offer_sales/price_offer_sales_items/ajax/price_offer_sales_items_table.blade.php
+++ resources/views/admin/sales/price_offer_sales/price_offer_sales_items/ajax/price_offer_sales_items_table.blade.php
@@ -4,9 +4,9 @@
         <th style="width: 40px"></th>
         <th>باركود</th>
         <th>اسم الصنف</th>
-        <th style="width: 100px">الكمية</th>
+{{--        <th style="width: 100px">الكمية</th>--}}
         <th style="width: 100px">السعر</th>
-        <th>المجموع</th>
+{{--        <th>المجموع</th>--}}
         <th>الملاحظات</th>
         <th style="width: 10px">العمليات</th>
     </tr>
@@ -24,17 +24,17 @@
                 </td>
                 <td class="text-center">{{ $key->product->barcode }}</td>
                 <td>{{ $key->product->product_name_ar }}</td>
-                <td>
-                    <input style="width: 80px;@if(!$key->qty == 0) background-color:palegoldenrod @endif;display: inline" class="form-control @if($key->qty == 0 || $key->qty == '') bg-danger @endif" onchange="update_qty_price_price_offer_sales_items_ajax({{ $key->id }},this.value,'qty')" type="text" value="{{ $key->qty }}">
-                    <div id="loader_qty_{{ $key->id }}" style="display: none" class="col text-center"><i style="font-size: 16px" class="fas fa-3x fa-sync-alt fa-spin"></i></div>
-                </td>
+{{--                <td>--}}
+{{--                    <input style="width: 80px;@if(!$key->qty == 0) background-color:palegoldenrod @endif;display: inline" class="form-control @if($key->qty == 0 || $key->qty == '') bg-danger @endif" onchange="update_qty_price_price_offer_sales_items_ajax({{ $key->id }},this.value,'qty')" type="text" value="{{ $key->qty }}">--}}
+{{--                    <div id="loader_qty_{{ $key->id }}" style="display: none" class="col text-center"><i style="font-size: 16px" class="fas fa-3x fa-sync-alt fa-spin"></i></div>--}}
+{{--                </td>--}}
                 <td>
                     <input style="width: 80px;background-color:palegoldenrod" class="form-control @if($key->price == 0 || $key->price == '') bg-danger @endif" onchange="update_qty_price_price_offer_sales_items_ajax({{ $key->id }},this.value,'price')" type="text" value="{{ $key->price }}">
                     <div id="loader_price_{{ $key->id }}" style="display: none" class="col text-center"><i style="font-size: 16px" class="fas fa-3x fa-sync-alt fa-spin"></i></div>
                 </td>
-                <td class="text-center">
-                    {{ $key->qty * $key->price }}
-                </td>
+{{--                <td class="text-center">--}}
+{{--                    {{ $key->qty * $key->price }}--}}
+{{--                </td>--}}
                 <td>
                     <textarea onchange="update_qty_price_price_offer_sales_items_ajax({{ $key->id }},this.value,'notes')" name="" class="form-control" id="" cols="30" rows="1" placeholder="اكتب ملاحظة ...">{{ $key->notes }}</textarea>
                     <div id="loader_notes_{{ $key->id }}" style="display: none" class="col text-center"><i style="font-size: 16px" class="fas fa-3x fa-sync-alt fa-spin"></i></div>
@@ -44,11 +44,11 @@
                 </td>
             </tr>
         @endforeach
-        <tr>
-            <td colspan="5" class="text-center bg-secondary">المجموع</td>
-            <td class="text-center" id="sum_items">{{ $sum }}</td>
-            <td></td>
-        </tr>
+{{--        <tr>--}}
+{{--            <td colspan="5" class="text-center bg-secondary">المجموع</td>--}}
+{{--            <td class="text-center" id="sum_items">{{ $sum }}</td>--}}
+{{--            <td></td>--}}
+{{--        </tr>--}}
     @else
         <tr>
             <td colspan="4" class="text-center">لا توجد بيانات</td>
